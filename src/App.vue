81ab340104e6924c9f6a33af30570595a7793102
Virtus-inc <aliondesh@gmail.com>
2023-05-29 15:09:37 +0300

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from  'vue'
+import modalUserInfo from './components/userModalDialog.vue'
+
+const isOpenDialog = ref(false);
+
+function setStatusDialog(value) {
+  isOpenDialog.value = value;
+}
+</script>
+
+<template>
+  <main>
+    <h1>Vue Component</h1>
+    <button @click="setStatusDialog(true)">Open Modal</button>
+
+    <modal-user-info v-if="isOpenDialog" :key="isOpenDialog.value" :is-active-dialog="isOpenDialog" @close-dialog="setStatusDialog">
+      <h3>My Modal Info</h3>
+    </modal-user-info>
+  </main>
+</template>
---
+++ src/components/modals/displayingUserInformation.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { defineProps, defineEmits, toRefs } from 'vue';
+const props = defineProps({
+  isActiveDialog: Function,
+});
+
+const { isActiveDialog } = toRefs(props);
+
+const emits = defineEmits({
+  closeDialog: false,
+});
+
+const setUpdateInfoModalDialog = () => {
+  emits('closeDialog', false);
+};
+</script>
+
+<template>
+  <div class="modal">
+    <div class="modal-inner">
+      <slot />
+      <button class="modal-close" @click="setUpdateInfoModalDialog">Close</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.modal {
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  z-index: 99;
+
+  background: rgba(0, 0, 0, 0.39);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.modal-inner {
+  background: #fff;
+  padding: 30px 80px 30px 80px;
+}
+</style>
